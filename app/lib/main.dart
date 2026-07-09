ec889057d4ce2e7b9547e0080fbbca76e8fe35e6
Mahad-siddiqui <m.mahadsiddiquii@gmail.com>
2026-07-10 00:29:44 +0500

    add initial Flutter boilerplate

---
+++ app/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Flutter Demo Home Page')),
+        body: Center(child: Text('Welcome to Flutter Demo!')),
+      ),
+    );
+  }
+}
